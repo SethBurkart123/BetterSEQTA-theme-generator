fbe7a8f26e8c7df00724330b9ed30f605080dedb
sethburkart123 <seth.burkart@hills.adventist.edu.au>
2024-06-10 17:52:35 +1000

    feat: create hacker theme

---
+++ postcss.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  plugins: [
+    require('postcss-mixins'),
+    require('autoprefixer'),
+    require('postcss-simple-vars'),
+    require('postcss-import'),
+    require('postcss-extend')
+  ]
+};
+
