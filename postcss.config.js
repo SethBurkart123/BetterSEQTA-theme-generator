488e8dd4e190917e71c9366a096edf484f156abc
sethburkart123 <seth.burkart@hills.adventist.edu.au>
2024-08-25 10:08:47 +1000

    add neumorphic theme

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,7 @@
 module.exports = {
+  syntax: 'postcss-scss',
   plugins: [
+    require('@csstools/postcss-sass'),
     require('postcss-mixins'),
     require('autoprefixer'),
     require('postcss-simple-vars'),
